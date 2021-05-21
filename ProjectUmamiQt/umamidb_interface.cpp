b8af4793f27f0716190294ac8abf714379d78f7b
Max-Person <max_person@mail.ru>
2021-05-21 18:31:46 +0300

    Реализованы функции UmamiDB_interface::getTitleBrowserByName() и UmamiDB_interface::getFranchiseBrowserByName()
    Реализованы методы класса UmamiDB_interface для поиска тайтлов и франшиз по их названию.
    Прим.: поскольку параметризированные запросы в данном контексте не поддерживаются (?), данная реализация подвержена SQL-инъекциям. Возможна необходимость обновления в будущем.

--- ProjectUmamiQt/umamidb_interface.cpp
+++ ProjectUmamiQt/umamidb_interface.cpp
@@ -43,6 +43,38 @@ QVector<TitleBrowserElement> UmamiDB_interface::getTitleBrowser()
     return browser;
 }
 
+QVector<TitleBrowserElement> UmamiDB_interface::getTitleBrowserByName(QString name)
+{
+    QSqlQuery query(umamiDB);
+    query.exec(
+    "select titles.idTitles, titles.Name, group_concat(genres.Name) as Genres, title_types.Name as Type, release_statuses.Name as Status \
+    from titles \
+        natural join genres_to_titles \
+        join genres  \
+            on genres_to_titles.idGenres=genres.idGenres \
+        join title_types \
+            on titles.TypeID = title_types.idTitleTypes \
+        join release_Statuses \
+            on titles.StatusID = release_Statuses.idReleaseStatuses \
+    where titles.Name like \"%" + name + "%\" \
+    group by titles.idTitles \
+    order by titles.Name asc;");
+
+    QVector<TitleBrowserElement> browser;
+    while (query.next()) {
+        TitleBrowserElement currentElement;
+        currentElement.id = query.value("idTitles").toInt();
+        currentElement.name = query.value("Name").toString();
+        currentElement.genres = query.value("Genres").toString();
+        currentElement.type = query.value("Type").toString();
+        currentElement.status = query.value("Status").toString();
+        browser.append(currentElement);
+    }
+
+    return browser;
+
+}
+
 QVector<FranchiseBrowserElement> UmamiDB_interface::getFranchiseBrowser()
 {
     QSqlQuery query(umamiDB);
@@ -51,6 +83,31 @@ QVector<FranchiseBrowserElement> UmamiDB_interface::getFranchiseBrowser()
     from franchises \
         join titles \
             on franchises.idfranchises=titles.FranchiseID \
+    group by franchises.idfranchises \
+    order by franchises.Name asc;");
+
+    QVector<FranchiseBrowserElement> browser;
+    while (query.next()) {
+        FranchiseBrowserElement currentElement;
+        currentElement.id = query.value("idfranchises").toInt();
+        currentElement.name = query.value("Name").toString();
+        currentElement.titles = query.value("Titles").toString();
+        browser.append(currentElement);
+    }
+
+    return browser;
+
+}
+
+QVector<FranchiseBrowserElement> UmamiDB_interface::getFranchiseBrowserByName(QString name)
+{
+    QSqlQuery query(umamiDB);
+    query.exec(
+    "select franchises.idfranchises, franchises.Name, group_concat(distinct quote(titles.Name)) as Titles \
+    from franchises \
+        join titles \
+            on franchises.idfranchises=titles.FranchiseID \
+    where franchises.Name like \"%" + name + "%\" \
     group by franchises.idfranchises \
     order by franchises.Name asc;");
 
